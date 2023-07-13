afbd10bc07e32758631f446006b5430bf3af9b57
naol86 <lijnaolkasinet@gmail.com>
2023-07-13 14:49:48 +0300

    complete

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ long int long_int_size;
 long long int long_long_int_size;
 float float_size;
 
-printf("size of a char: %zu byte(s)\n", sizeof(char_size));
+printf("Size of a char: %zu byte(s)\n", sizeof(char_size));
 printf("Size of an int: %zu byte(s)\n", sizeof(int_size));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_size));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_size));
