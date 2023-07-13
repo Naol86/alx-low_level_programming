3b2c08d2072419fa9c5e8e6aaa1daeb1516931df
naol86 <lijnaolkasinet@gmail.com>
2023-07-13 14:46:37 +0300

    easy

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - size
+ * Return: 0
+ */
+int main(void)
+{
+char char_size;
+int int_size;
+long int long_int_size;
+long long int long_long_int_size;
+float float_size;
+
+printf("size of a char: %zu byte(s)\n", sizeof(char_size));
+printf("Size of an int: %zu byte(s)\n", sizeof(int_size));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_size));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_size));
+printf("Size of a float: %zu byte(s)\n", sizeof(float_size));
+return (0);
+}
