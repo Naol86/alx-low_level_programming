c6796585adce5e27f6539f5692d4027dbf24f9cb
naol86 <lijnaolkasinet@gmail.com>
2023-07-14 21:22:06 +0300

    test

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,22 @@
 
 int main(void)
 {
-int i = 48;
-int j;
-for (; i < 58; i++)
-{
-j = i + 1;
-for (; j < 58; j++)
-{
-putchar(i);
-putchar(j);
-if (i < 56)
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-putchar(32);
-return (0);
+    int i = 48;
+    int j;
+    for (; i < 58; i++)
+    {
+        j = i + 1;
+        for (; j < 58; j++)
+        {
+            putchar(i);
+            putchar(j);
+            if (i < 56)
+            {
+                putchar(44);
+                putchar(32);
+            }
+        }
+    }
+    putchar(32);
+    return (0);
 }
