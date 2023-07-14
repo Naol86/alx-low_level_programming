216829c1c2cb2c41b6c5f28fb189ec63dd618aed
naol86 <lijnaolkasinet@gmail.com>
2023-07-14 23:29:02 +0300

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - combination
- *
- * Return: 0
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num;
-int num1;
-int num2;
-int num3;
+int i, j;
 
-for (num = 0; num < 10; num++)
+for (i = 0; i < 100; i++)
 {
-for (num1 = 0; num1 < 10; num1++)
+for (j = 0; j < 100; j++)
 {
-for (num2 = 0; num2 < 10; num2++)
+if (i < j)
 {
-for (num3 = num1 + 1; num3 < 10; num3++)
-{
-putchar((num % 10) + '0');
-putchar((num1 % 10) + '0');
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar((num2 % 10) + '0');
-putchar((num3 % 10) + '0');
-if (num1 == 8 && num == 9 && num2 == 9)
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
-break;
-}
 putchar(',');
 putchar(' ');
 }
 }
-if (num == 9 && num1 == 8 && num2 == 9)
-{
-break;
 }
 }
-if (num == 7 && num1 == 8 && num2 == 9)
-{
-break;
-}
-}
-putchar(10);
+putchar('\n');
 return (0);
 }
