4b94160f987953a511ebad5fcf955b1051cbc97f
naol86 <lijnaolkasinet@gmail.com>
2023-07-19 12:43:27 +0300

    test

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sum - print summation
+ */
+void print_sum(void)
+{
+int sum = 0;
+int i = 0;
+for (; i < 1024; i++)
+{
+if (i % 3 == 0)
+{
+sum += i;
+continue;
+}
+else if (i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("d%\n", sum);
+}
