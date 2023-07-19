d3cf49ade76ff669b0273681376e3564ff195429
naol86 <lijnaolkasinet@gmail.com>
2023-07-19 13:20:04 +0300

    complete

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@ long int a = 1;
 long int b = 1;
 int i = 0;
 long int temp;
-for (; i < 50; i++)
+for (; i <= 50; i++)
 {
 if (i == 0 || i == 1)
 printf("%ld", a);
@@ -20,7 +20,7 @@ temp = a + b;
 a = b;
 b = temp;
 }
-if (i != 49)
+if (i != 50)
 {
 printf("%c", ',');
 printf("%c", ' ');
