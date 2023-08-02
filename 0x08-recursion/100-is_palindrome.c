ec1b61fbc920333681b8328d8287fe8442f7ed73
naol86 <lijnaolkasinet@gmail.com>
2023-08-02 20:03:37 +0300

    test for 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,29 +10,39 @@ int length(char *s)
 {
 int x = 0;
 while (s[x] != '\0')
-{
 x++;
-}
 return (x);
 }
 
+/**
+ * check - check string
+ * @s: string
+ * @start: int
+ * @end: int
+ * Return: int
+ */
+
+int check(char *s, int start, int end)
+{
+if (start > end)
+return (1);
+if (s[start] != s[end])
+return (0);
+if (start == end)
+return (1);
+start++;
+end--;
+return (check(s, start, end));
+}
 /**
  * is_palindrome - check if a string is a palindrome
  * @s: char
  * Return: int
  */
-
 int is_palindrome(char *s)
 {
-int len = length(s);
+int len = length(s) - 1;
 int mid = len / 2;
-int x = 0;
-len--;
-while (x < mid)
-{
-if (s[x] != s[len - x])
-return (0);
-x++;
-}
-return (1);
+int ans = check(s, 0, len);
+return (ans);
 }
--- 0x08-recursion/test.c
+++ 0x08-recursion/test.c
@@ -4,24 +4,28 @@
 int length(char *s){
     int x=0;
     while (s[x]!='\0')
-    {
         x++;
-    }
     return (x);
+}
+int check(char *s,int start,int end)
+{
+    if (start > end)
+        return (1);
+    if (s[start]!=s[end])
+        return (0);
+    if (start == end )
+        return (1);
+    start++;
+    end--;
+    return (check(s,start,end));
 }
 
 int is_palindrome(char *s)
 {
-    int len = length(s);
+    int len = length(s) - 1;
     int mid = len/2;
-    int x=0;
-    len--;
-    while(x<mid){
-        if (s[x]!=s[len-x])
-            return (0);
-        x++;
-    }
-    return (1);
+    int ans = check(s,0,len);
+    return (ans);
 }
 
 int main(void)
