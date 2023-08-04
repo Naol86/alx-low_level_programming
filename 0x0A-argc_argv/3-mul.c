e40cfe0fdc1aa0cd3e1cfd8a439538a60ea918de
naol86 <lijnaolkasinet@gmail.com>
2023-08-04 12:55:21 +0300

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - multiplication
+ * @argc: int
+ * @argv: argument
+ * Return: int 0
+ */
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    
+int i;
+int temp;
+int ans = 1;
+for (i = 1; i < argc; i++)
+{
+temp = atoi(argv[i]);
+ans = ans *temp;
+}
+if (i == 3)
+printf("%d\n", ans);
+else
+printf("Error\n");
+return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc,char* argv[])
+{
+    int a=atoi("e");
+    printf("%d\n",a);
+}
