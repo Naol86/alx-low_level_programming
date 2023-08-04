e11ceb2e3a05a89e2a9b61f8ac3bba465b987818
naol86 <lijnaolkasinet@gmail.com>
2023-08-04 21:36:29 +0300

    test 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    int a=atoi("e");
-    printf("%d\n",a);
+    int i;
+    int temp;
+    int ans = 0;
+    for (i = 1; i < argc; i++)
+    {
+        temp = atoi(argv[i]);
+        if (temp == 0 && *argv[i] != '0')
+        {
+            printf("Error\n");
+            return (0);
+        }
+        ans += temp;
+    }
+    if (argc>1)
+        printf("%d\n", ans);
+    else
+        printf("%d\n",0);
+    return (0);
 }
