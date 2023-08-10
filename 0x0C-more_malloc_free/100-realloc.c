8a437a48374bc13b6b78f8ea3334f8a27ea66441
naol86 <lijnaolkasinet@gmail.com>
2023-08-10 22:25:51 +0300

    done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *new_ptr1;
+char *new_ptr;
+char *old_ptr;
+unsigned int i;
+
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+new_ptr1 = malloc(new_size);
+return (new_ptr1);
+}
+new_ptr = malloc(new_size);
+
+if (!new_ptr)
+return (NULL);
+
+old_ptr = ptr;
+
+if (new_size < old_size)
+{
+for (i = 0; i < new_size; i++)
+new_ptr[i] = *(old_ptr + i);
+}
+
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+new_ptr[i] = *(old_ptr + i);
+}
+
+free(ptr);
+return (new_ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,10 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+
 
 #endif
