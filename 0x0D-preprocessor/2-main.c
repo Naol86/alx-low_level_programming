1b29b250b1f92097c0ecf40a4308f060b572e74f
naol86 <lijnaolkasinet@gmail.com>
2023-08-14 23:04:26 +0300

    test

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * @argc: int
+ * @argv: parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+printf("%s", __FILE__);
+return (0);
+}
