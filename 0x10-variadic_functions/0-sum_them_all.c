af1440fd3241cb3691a070acbb60b00baf627052
naol86 <lijnaolkasinet@gmail.com>
2023-08-17 12:52:25 +0300

    test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,19 +4,22 @@
 /**
  * sum_them_all - summation of all arguments
  * @n: size of argument
- * Return: int
+ * Return: the summation
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int summation = 0;
 unsigned int i;
+unsigned int temp;
 va_list list;
 va_start(list, n);
 
 for (i = 0; i < n; i++)
-summation += va_arg(list, int);
-
+{
+temp = va_arg(list, int);
+summation += temp;
+}
 va_end(list);
 
 return (summation);
