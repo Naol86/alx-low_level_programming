e7379f3a1224dd1cc737f1403da1b4987c380ea3
naol86 <lijnaolkasinet@gmail.com>
2023-08-24 17:03:37 +0300

    test

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,20 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current;
 	list_t *temp;
 
-	if (str == NULL)
-		return (NULL);
-	temp = malloc(sizeof(list_t));
-
-	if (temp == NULL)
-		return (NULL);
 	temp->str = strdup(str);
-
-	if (temp->str == NULL)
-	{
-		free(temp);
-		return (NULL);
-	}
-
 	temp->len = str_len(temp->str);
 	temp->next = NULL;
 
