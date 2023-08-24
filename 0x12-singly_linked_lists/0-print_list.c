d4a1cacec9939f4ed8edf8db0f5b168e48674803
naol86 <lijnaolkasinet@gmail.com>
2023-08-24 15:51:10 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - print list element in singly linked list
  * @h: linked list
@@ -9,13 +10,13 @@ size_t print_list(const list_t *h)
 	list_t *current = h;
 	unsigned int count = 0;
 
-	if (count == NULL)
+	if (current == NULL)
 	{
 		printf("[0] (nil)");
 	}
 	while (current != NULL)
 	{
-		printf("[%ul] %s\n", current->len, current->str);
+		printf("[%u] %s\n", current->len, current->str);
 		current = current->next;
 		count++;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,4 +22,4 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
-#endif
+#endif /* _LIST_H_ */
