962c415d60d5f8707a768ae5d718f54f451ffab8
naol86 <lijnaolkasinet@gmail.com>
2023-08-24 18:53:48 +0300

    test

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - free memory
+ * @head: linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	current = head;
+
+	if (current == NULL)
+		return;
+	free_list(current->next);
+	free(current->str);
+	free(current);
+}
