02b0e12a60950f7151548fca7a87372f438f741a
naol86 <lijnaolkasinet@gmail.com>
2023-08-24 19:16:03 +0300

    test

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void print_something(void) __attribute__((constructor));
+/**
+ * print_something - print before main
+ */
+void print_something(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
