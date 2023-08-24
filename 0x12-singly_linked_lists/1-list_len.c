063555b7b4ab604e618fa8bd9c55af615a12d666
naol86 <lijnaolkasinet@gmail.com>
2023-08-24 16:39:52 +0300

    test

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,7 +6,7 @@
  */
 size_t list_len(const list_t *h)
 {
-	list_t *current;
+	const list_t *current;
 	unsigned int count = 0;
 
 	current = h;
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * str_len - length of string
+ * @str: string
+ * Return: unsigned int
+ */
 unsigned int str_len(char *str)
 {
 	unsigned int len = 0;
@@ -8,7 +12,12 @@ unsigned int str_len(char *str)
 		len++;
 	return (len);
 }
-
+/**
+ * add_node - add node at the beginning
+ * @head: head of the node
+ * @str: string
+ * Return: list_t*
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
