2fe1cf214c7d56859005d7a5ba062bba1f32bae9
naol86 <lijnaolkasinet@gmail.com>
2023-08-31 21:56:02 +0300

    test for 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - change into 0
+ * @n: number
+ * @index: index
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int temp = 1 << index, ans;
+
+	if (index > 32 || temp > *n)
+		return (-1);
+
+	ans = (*n | temp) - temp;
+	*n = ans;
+	return (1);
+}
