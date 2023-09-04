08b81644db5d455a96e831513146dca943c056ab
naol86 <lijnaolkasinet@gmail.com>
2023-09-04 21:52:17 +0300

    test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,8 +12,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	
-	if(!text_content)
+
+	if (!text_content)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
