74c38b52877388999c12af615059ae237ccd2e6c
naol86 <lijnaolkasinet@gmail.com>
2023-09-04 20:25:33 +0300

    test

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - read from file
+ * @filename: name of the file
+ * @letters: size of letters to read
+ * Return: ssize_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, sizeof_char, post;
+	char *str;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file < 0)
+		return (0);
+
+	str = malloc(sizeof(char) * letters);
+
+	if (!str)
+		return (0);
+
+	sizeof_char = read(file, str, letters);
+
+	if (sizeof_char < 0)
+	{
+		free(sizeof_char);
+		return (0);
+	}
+
+	str[sizeof_char] = '\0';
+
+	close(file);
+
+	post = write(STDOUT_FILENO, str, sizeof_char);
+
+	if (post < 0)
+	{
+		free(str);
+		return (0);
+	}
+
+	free(str);
+	return (post);
+}
