b04f96aabd8628dd68d5addc0183180356fb1110
naol86 <lijnaolkasinet@gmail.com>
2023-10-13 22:44:02 +0300

    test

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ * @head: head of the list
+ * @index: index
+ * Return: dlistint_t*
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int indx = 0;
+
+	current = head;
+
+	while (current->next)
+	{
+		if (indx == index)
+			return (current);
+		current = current->next;
+		indx++;
+	}
+
+	return (NULL);
+}
