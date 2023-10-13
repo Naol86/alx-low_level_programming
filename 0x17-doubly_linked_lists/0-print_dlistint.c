aca63119dfba876803e7de0ec5325930a29ee2e4
naol86 <lijnaolkasinet@gmail.com>
2023-10-13 22:03:01 +0300

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,8 +8,8 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *current;
-	unsigned int count = 0;
+	const dlistint_t *current;
+	size_t count = 0;
 
 	current = h;
 	if (current != NULL)
@@ -18,7 +18,7 @@ size_t print_dlistint(const dlistint_t *h)
 			printf("%d\n", current->n);
 			current = current->next;
 			count++;
-		} while (current != h);
+		} while (current);
 	}
 	return (count);
 }
