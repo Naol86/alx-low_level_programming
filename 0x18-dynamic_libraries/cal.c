9c0b49881fb1bb32c0334465d563100858d75535
naol86 <lijnaolkasinet@gmail.com>
2023-10-17 05:37:45 +0300

    test

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - add two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	int ans;
+	ans = a + b;
+	return (ans);
+}
+/**
+ * sub - difference of a number
+ * @a: int a
+ * @b: int b
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	int ans;
+	ans = a - b;
+	return (ans);
+}
+
+/**
+ * mul - multiplication of two numbers
+ * @a: int a
+ * @b: int b
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	int ans;
+	ans = a * b;
+	return (ans);
+}
+
+/**
+ * div - divison of a number
+ * @a: int a
+ * @b: int b 
+ * Return: int
+ */
+int div(int a, int b)
+{
+	int ans;
+	ans = a / b;
+	return (ans);
+}
+
+/**
+ * mode - mode of a number
+ * @a: int a
+ * @b: int b
+ * Return: int
+ */
+int mode(int a, int b)
+{
+	int ans;
+	ans = a % b;
+	return (ans);
+}
