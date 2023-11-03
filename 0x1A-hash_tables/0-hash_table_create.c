fc3784ff406b129d0ef15f8dbacaad54a9f3be55
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 17:56:41 +0300

    test

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	table->size = size;
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+
+	return (table);
+}
