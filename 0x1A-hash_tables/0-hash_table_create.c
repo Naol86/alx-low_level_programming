2ead617eed433d860d522e42c423d62368c2ea92
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 18:21:47 +0300

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 
