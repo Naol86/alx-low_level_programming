79704fc9fabf744e8ade5091474725a171702912
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 18:14:38 +0300

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL){
+		free(table);
+		return (NULL);
+	}
 
 	return (table);
 }
