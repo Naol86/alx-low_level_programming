25530e0895b4a5a74492903d31789edebec65f15
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 21:16:33 +0300

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,73 +1,82 @@
 #include "hash_tables.h"
-
 /**
- * make_hash_node - creates a new hash node
- * @key: key for the node
- * @value: for the node
+ * hash_table_set - insert new data
+ * @ht: hash table
+ * @key: the data to be translated to index by the hash function
+ * @value: is the data that is ascociated with the key
  *
- * Return: the new node, or NULL on failure
- */
-hash_node_t *make_hash_node(const char *key, const char *value)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (NULL);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	node->next = NULL;
-	return (node);
-}
-
-
-/**
- * hash_table_set - sets a key to a value in the hash table
- * @ht: hash table to add elemt to
- * @key: key for the data
- * @value: data to store
- *
- * Return: 1 if successful, 0 otherwise
- */
+ * Return: 1 if success 0 if not
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *hash_node, *tmp;
-	char *new_value;
+	hash_node_t *new_node = NULL;
+	hash_node_t *current_node = NULL;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	key == NULL || strlen(key) == 0 || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
+
+	index = key_index((unsigned char *) key, ht->size);
+	current_node = ht->array[index];
+
+	for (; current_node != NULL; current_node = current_node->next)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if (strcmp(current_node->key, key) == 0)
 		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
+			if (strcmp(current_node->value, value) != 0)
+			{
+				free(current_node->value);
+				current_node->value = strdup(value);
+			}
+		return (1);
 		}
-		tmp = tmp->next;
 	}
-	hash_node = make_hash_node(key, value);
-	if (hash_node == NULL)
+
+	new_node = New_Node(key, value);
+	if (new_node == NULL)
 		return (0);
-	hash_node->next = ht->array[index];
-	ht->array[index] = hash_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
 	return (1);
+}
+
+/**
+ * New_Node - new node
+ * @key: pointer to key.
+ * @value: pointer to value of the key.
+ *
+ * Return: pointer to new_node node else NULL.
+ */
+hash_node_t *New_Node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,6 +36,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-hash_node_t *make_hash_node(const char *key, const char *value);
+hash_node_t *New_Node(const char *key, const char *value);
 
 #endif
