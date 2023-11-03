86c5b7846f2e6d3cfd527a8b0d35417d16abc38b
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 18:00:34 +0300

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creating hash table
+ * @size: size of the
+ * Return: hash_table_t*
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
